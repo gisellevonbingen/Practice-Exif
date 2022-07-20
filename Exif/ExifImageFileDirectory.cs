9dd8c57a5d256d93a32e64f2fed18be7cc71a966
gisellevonbingen <gisellevonbingen@gmail.com>
2022-07-20 13:24:18 +0900

    Update ExifImageFileDirectory.cs

--- Exif/ExifImageFileDirectory.cs
+++ Exif/ExifImageFileDirectory.cs
@@ -47,6 +47,73 @@ namespace Exif
 
         }
 
+        public bool TryGetIntegers(ExifTagId id, out IExifValueIntegers integers)
+        {
+            if (this.TryGetValue(id, out var value) == true && value is IExifValueIntegers _integers)
+            {
+                integers = _integers;
+                return true;
+            }
+            else
+            {
+                integers = null;
+                return false;
+            }
+
+        }
+
+        public int GetSigned(ExifTagId id, int fallback = default)
+        {
+            if (this.TryGetIntegers(id, out var integers) == true)
+            {
+                return integers.AsSigned;
+            }
+            else
+            {
+                return fallback;
+            }
+
+        }
+
+        public uint GetUnsigned(ExifTagId id, uint fallback = default)
+        {
+            if (this.TryGetIntegers(id, out var integers) == true)
+            {
+                return integers.AsUnsigned;
+            }
+            else
+            {
+                return fallback;
+            }
+
+        }
+
+        public int[] GetSigneds(ExifTagId id)
+        {
+            if (this.TryGetIntegers(id, out var integers) == true)
+            {
+                return integers.AsSigneds.ToArray();
+            }
+            else
+            {
+                return new int[0];
+            }
+
+        }
+
+        public uint[] GetUnsigneds(ExifTagId id)
+        {
+            if (this.TryGetIntegers(id, out var integers) == true)
+            {
+                return integers.AsUnsigneds.ToArray();
+            }
+            else
+            {
+                return new uint[0];
+            }
+
+        }
+
     }
 
 }
